2cc7c3960b4b3284af7ee0a89d7e7873db6109de
Dexter <na>
2019-05-01 00:47:43 +0100

    Initial set up of DynamoDB connection from app to AWS

--- PLNKTN/PLNKTN/Controllers/UserDataController.cs
+++ PLNKTN/PLNKTN/Controllers/UserDataController.cs
@@ -3,10 +3,17 @@ using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
+using Amazon;
+using Amazon.DynamoDBv2;
+using Amazon.DynamoDBv2.DataModel;
+using Amazon.DynamoDBv2.Model;
+using Amazon.Runtime;
+using Amazon.Runtime.CredentialManagement;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
 using PLNKTN.DTOs;
+using PLNKTN.Models;
 
 namespace PLNKTN.Controllers
 {
@@ -44,9 +51,39 @@ namespace PLNKTN.Controllers
 
         // DELETE: api/UserData/5
         [HttpDelete("{id}")]
-        public IActionResult Delete(int id)
+        public async Task<IActionResult> Delete(int id)
         {
+            //Task<IActionResult>
+            await ExecuteAsync();
             return BadRequest("userdata DELETE is not implemented");
+        }
+
+        public static async Task ExecuteAsync()
+        {
+            var chain = new CredentialProfileStoreChain();
+            AWSCredentials awsCredentials;
+            if (chain.TryGetAWSCredentials("Dexter", out awsCredentials))
+            {
+                var a = awsCredentials.GetCredentials();
+                AmazonDynamoDBConfig clientConfig = new AmazonDynamoDBConfig();
+                // This client will access the US West 1 region (N Cali).
+                clientConfig.RegionEndpoint = RegionEndpoint.USWest1;
+                AmazonDynamoDBClient client = new AmazonDynamoDBClient(a.AccessKey, a.SecretKey, clientConfig);
+
+                using (var context = new DynamoDBContext(client))
+                {
+                    User user = new User
+                    {
+                        Email = "b@b.com",
+                        First_name = "d",
+                        Last_name = "c",
+                        Created_at = "20190430T122330Z",
+                        Level = "L2"
+                    };
+
+                    await context.SaveAsync(user);
+                }
+            }
         }
     }
 }
--- PLNKTN/PLNKTN/Models/User.cs
+++ PLNKTN/PLNKTN/Models/User.cs
@@ -1,17 +1,19 @@
 ﻿using System;
+using Amazon.DynamoDBv2.DataModel;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace PLNKTN.Models
 {
+    [DynamoDBTable("Users")]
     public class User
     {
-        public int Id { get; set; }
+        [DynamoDBHashKey]
+        public string Email { get; set; }
         public string First_name { get; set; }
         public string Last_name { get; set; }
-        public string Email { get; set; }
-        public DateTime Created_at { get; set; }
+        public string Created_at { get; set; }
         public string Level { get; set; }
 
         public User()
