5e0f53d45c7efe9058f6529a5a12a887d58380e4
Dexter Cunningham <dextercunningham@hotmail.co.uk>
2020-02-07 15:14:46 +0000

    Refactor Message Creation in the Periodic Execution App
    Changed the code to provide readonly properties for unchanging and reused values.  Modularised and made efficiency improvements to string generation.

--- PLNKTN/PeriodicExecutionApp/Program.cs
+++ PLNKTN/PeriodicExecutionApp/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Net.Http;
 using System.Net.Http.Headers;
+using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -8,22 +9,24 @@ namespace PeriodicExecutionApp
 {
     class Program
     {
+        private static string _serverUri { get; } = "http://ec2-54-241-148-226.us-west-1.compute.amazonaws.com/";
+        private static string _localUri { get; } = "https://localhost:44312/";
+        private static string _newLine { get; } = Environment.NewLine;
+
         static void Main(string[] args)
         {
             var rewardsControllerUri = "api/Rewards";
 
-            Console.WriteLine("This is the console app to execute the following methods from the PLNKTN Web App:\n" +
-                "Calculate User Challange completion\n" +
-                "Calculate User Reward completion\n");
+            Console.WriteLine(CreateIntroMsg());
 
-            // For debug - waits for server to start up properly before calling
+            // Waits for server to start up properly before calling (in case it has been put in standby)
             Thread.Sleep(1000);
 
             ExecuteAPICall("CalcChallenges", rewardsControllerUri, "CalculateUserChallengeCompletion()").Wait();
 
             // Waits 20s for DynamoDb to synchronise any writes from the previous call.  Shouldn't be needed as consistent read is set
             // however this doesn't seem to be working.  20s is the AWS stipulated period of time.
-            Console.WriteLine("\nWaiting 20 seconds...");
+            Console.WriteLine(_newLine + "Waiting 20 seconds...");
             Thread.Sleep(20000);
 
             ExecuteAPICall("CalcRewards", rewardsControllerUri, "CalculateUserRewardCompletion()").Wait();
@@ -36,8 +39,9 @@ namespace PeriodicExecutionApp
         {
             HttpClient httpClient = new HttpClient
             {
-                BaseAddress = new Uri("http://ec2-54-241-148-226.us-west-1.compute.amazonaws.com/")
-                // For LOCAL ONLY -> BaseAddress = new Uri("https://localhost:44312/")
+                BaseAddress = new Uri(_serverUri)
+                // For LOCAL ONLY -> 
+                //BaseAddress = new Uri(_localUri)
             };
             httpClient.DefaultRequestHeaders.Accept.Clear();
             httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
@@ -51,11 +55,43 @@ namespace PeriodicExecutionApp
                 res.EnsureSuccessStatusCode();
                 if (res.IsSuccessStatusCode)
                 {
-                    var s = await res.Content.ReadAsStringAsync();
-                    Console.WriteLine("\n" + "The API method '" + apiCallName + "' at location '" + apiURL + "' has been executed.\n" + "API Content Output: " + s + "\n" +
-                        "API HTTP Response: " + res.StatusCode.ToString());
+                    var apiResponse = await res.Content.ReadAsStringAsync();
+                    Console.WriteLine(CreateAPIOutputMsg(apiURL, apiCallName, apiResponse, res.StatusCode.ToString()));
                 }
             }
+        }
+
+        private static string CreateIntroMsg()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.Append("This is the console app to execute the following methods from the PLNKTN Web App:");
+            sb.Append(_newLine);
+            sb.Append("Calculate User Challange completion");
+            sb.Append(_newLine);
+            sb.Append("Calculate User Reward completion");
+            sb.Append(_newLine);
+
+            return sb.ToString();
+        }
+
+        private static string CreateAPIOutputMsg(string apiURL, string apiCallName, string apiResponse, string statusCode)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.Append("The API method '");
+            sb.Append(apiCallName);
+            sb.Append("' at location '");
+            sb.Append(apiURL);
+            sb.Append("' has been executed.");
+            sb.Append(_newLine);
+            sb.Append("API Content Output: ");
+            sb.Append(apiResponse);
+            sb.Append(_newLine);
+            sb.Append("API HTTP Response: ");
+            sb.Append(statusCode);
+
+            return sb.ToString();
         }
     }
 }
