e2837ee289c0d6843ef8ba2ed683b03c4e5a7cbc
Dexter Cunningham <dextercunningham@hotmail.co.uk>
2020-02-10 22:29:17 +0000

    Refactor ExecuteAPICall to Use Global HttpClient Object
    A global HttpClient object is used as this is best practice in .NET as it acts as a session to send HTTP requests.  Errors can occur if multiple instantiations are used.

--- PLNKTN/PeriodicExecutionApp/Program.cs
+++ PLNKTN/PeriodicExecutionApp/Program.cs
@@ -11,53 +11,49 @@ namespace PeriodicExecutionApp
     {
         private static string _serverUri { get; } = "http://ec2-54-241-148-226.us-west-1.compute.amazonaws.com/";
         private static string _localUri { get; } = "https://localhost:44312/";
+        private static string _rewardsControllerUri { get; } = "api/Rewards";
         private static string _newLine { get; } = Environment.NewLine;
+        private static HttpClient _httpClient { get; } = new HttpClient();
 
         static void Main(string[] args)
         {
-            var rewardsControllerUri = "api/Rewards";
-
             Console.WriteLine(CreateIntroMsg());
 
             // Waits for server to start up properly before calling (in case it has been put in standby)
             Thread.Sleep(1000);
 
-            ExecuteAPICall("CalcChallenges", rewardsControllerUri, "CalculateUserChallengeCompletion()").Wait();
+            ExecuteAPICall("CalcChallenges", _rewardsControllerUri, "CalculateUserChallengeCompletion()").Wait();
 
             // Waits 20s for DynamoDb to synchronise any writes from the previous call.  Shouldn't be needed as consistent read is set
             // however this doesn't seem to be working.  20s is the AWS stipulated period of time.
             Console.WriteLine(_newLine + "Waiting 20 seconds...");
             Thread.Sleep(20000);
 
-            ExecuteAPICall("CalcRewards", rewardsControllerUri, "CalculateUserRewardCompletion()").Wait();
+            ExecuteAPICall("CalcRewards", _rewardsControllerUri, "CalculateUserRewardCompletion()").Wait();
 
-            // TODO - Remove from release version
-            Console.ReadLine();
+            // TODO - Implement logging to record web API return values
+            Thread.Sleep(10000);
+            return;
         }
 
         static async Task ExecuteAPICall(string httpRequestVerb, string apiURL, string apiCallName)
         {
-            HttpClient httpClient = new HttpClient
-            {
-                BaseAddress = new Uri(_serverUri)
-                // For LOCAL ONLY -> 
-                //BaseAddress = new Uri(_localUri)
-            };
-            httpClient.DefaultRequestHeaders.Accept.Clear();
-            httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            _httpClient.BaseAddress = new Uri(_serverUri);
+            // For LOCAL ONLY -> 
+            // _httpClientBaseAddress = new Uri(_localUri);
 
-            using (httpClient)
+            _httpClient.DefaultRequestHeaders.Accept.Clear();
+            _httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+            // Create a custom HTTP request message and use it to call the API method
+            var customMethod = new HttpMethod(httpRequestVerb);
+            var requestMessage = new HttpRequestMessage(customMethod, apiURL);
+            HttpResponseMessage res = await _httpClient.SendAsync(requestMessage);
+            res.EnsureSuccessStatusCode();
+            if (res.IsSuccessStatusCode)
             {
-                // Create a custom HTTP request message and use it to call the API method
-                var customMethod = new HttpMethod(httpRequestVerb);
-                var requestMessage = new HttpRequestMessage(customMethod, apiURL);
-                HttpResponseMessage res = await httpClient.SendAsync(requestMessage);
-                res.EnsureSuccessStatusCode();
-                if (res.IsSuccessStatusCode)
-                {
-                    var apiResponse = await res.Content.ReadAsStringAsync();
-                    Console.WriteLine(CreateAPIOutputMsg(apiURL, apiCallName, apiResponse, res.StatusCode.ToString()));
-                }
+                var apiResponse = await res.Content.ReadAsStringAsync();
+                Console.WriteLine(CreateAPIOutputMsg(apiURL, apiCallName, apiResponse, res.StatusCode.ToString()));
             }
         }
 
