4766d9d9b5dabac2ae89dd2145210f2ea8432374
Dexter Cunningham <dextercunningham@hotmail.co.uk>
2020-02-21 19:35:39 +0000

    Small Refactor of Periodic Execution App and add Returns to Challenge and Reward Calc API Methods

--- PLNKTN/PeriodicExecutionApp/Program.cs
+++ PLNKTN/PeriodicExecutionApp/Program.cs
@@ -7,43 +7,61 @@ using System.Threading.Tasks;
 
 namespace PeriodicExecutionApp
 {
-    class Program
+    public class Program
     {
         private static string _serverUri { get; } = "http://ec2-54-241-148-226.us-west-1.compute.amazonaws.com/";
         private static string _localUri { get; } = "https://localhost:44312/";
         private static string _rewardsControllerUri { get; } = "api/Rewards";
         private static string _newLine { get; } = Environment.NewLine;
-        private static HttpClient _httpClient { get; } = new HttpClient();
+        private static HttpClient _httpClient { get; set; } = new HttpClient();
 
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
             Console.WriteLine(CreateIntroMsg());
 
-            // Waits for server to start up properly before calling (in case it has been put in standby)
+            // Wait for server to start up properly before calling (in case it has been put in standby)
             Thread.Sleep(1000);
 
-            ExecuteAPICall("CalcChallenges", _rewardsControllerUri, "CalculateUserChallengeCompletion()").Wait();
+            _httpClient = SetupHttpClient(_httpClient);
+
+            Task calcChallenges = ExecuteAPICall("CalcChallenges", _rewardsControllerUri, "CalculateUserChallengeCompletion()");
+            Console.WriteLine(_newLine + "Calculating challenge completion...");
+            calcChallenges.Wait();
+            Console.WriteLine(_newLine + "Challenge calculations complete!");
 
             // Waits 20s for DynamoDb to synchronise any writes from the previous call.  Shouldn't be needed as consistent read is set
             // however this doesn't seem to be working.  20s is the AWS stipulated period of time.
             Console.WriteLine(_newLine + "Waiting 20 seconds...");
             Thread.Sleep(20000);
 
+            
+
             ExecuteAPICall("CalcRewards", _rewardsControllerUri, "CalculateUserRewardCompletion()").Wait();
 
             // TODO - Implement logging to record web API return values
             Thread.Sleep(10000);
-            return;
         }
 
-        static async Task ExecuteAPICall(string httpRequestVerb, string apiURL, string apiCallName)
+        private static HttpClient SetupHttpClient(HttpClient httpClient)
         {
-            _httpClient.BaseAddress = new Uri(_serverUri);
+            httpClient.BaseAddress = new Uri(_serverUri);
             // For LOCAL ONLY -> 
-            // _httpClientBaseAddress = new Uri(_localUri);
-            _httpClient.DefaultRequestHeaders.Accept.Clear();
-            _httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            // httpClient.BaseAddress = new Uri(_localUri);
+            httpClient.DefaultRequestHeaders.Accept.Clear();
+            httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            return httpClient;
+        }
 
+        /// <summary>
+        /// Uses a specified URL, method name and HTTP verb to call a Web API method where it will then
+        /// check the status code.  If the code is success 
+        /// </summary>
+        /// <param name="httpRequestVerb">The HTTP verb to be called.  Can be custom</param>
+        /// <param name="apiURL">The URL or the API to be called</param>
+        /// <param name="apiCallName">The method name in the specified API to be called</param>
+        /// <returns>Task operation</returns>
+        private static async Task ExecuteAPICall(string httpRequestVerb, string apiURL, string apiCallName)
+        {
             // Create a custom HTTP request message and use it to call the API method
             HttpMethod customMethod = new HttpMethod(httpRequestVerb);
             HttpRequestMessage requestMessage = new HttpRequestMessage(customMethod, apiURL);
@@ -62,10 +80,19 @@ namespace PeriodicExecutionApp
             catch (HttpRequestException ex)
             {
                 // TODO - Implement logging
-                throw;
+                throw ex;
+            }
+            catch (Exception ex)
+            {
+                // TODO - Implement logging
+                throw new InvalidOperationException("An error has occurred during execution of " + apiCallName + " method.", ex);
             }
         }
 
+        /// <summary>
+        /// Builds the informational message to be displayed on the console / logged.
+        /// </summary>
+        /// <returns>String object with informational message</returns>
         private static string CreateIntroMsg()
         {
             StringBuilder sb = new StringBuilder();
@@ -80,6 +107,14 @@ namespace PeriodicExecutionApp
             return sb.ToString();
         }
 
+        /// <summary>
+        /// Builds Web API return information message to be displayed to the user / logged.
+        /// </summary>
+        /// <param name="apiURL">URL of the called API</param>
+        /// <param name="apiCallName">The method name in the specified API to be called</param>
+        /// <param name="apiResponse">Response message received from the API</param>
+        /// <param name="statusCode">Status code received from teh API</param>
+        /// <returns>String object with informational message</returns>
         private static string CreateAPIOutputMsg(string apiURL, string apiCallName, string apiResponse, string statusCode)
         {
             StringBuilder sb = new StringBuilder();
