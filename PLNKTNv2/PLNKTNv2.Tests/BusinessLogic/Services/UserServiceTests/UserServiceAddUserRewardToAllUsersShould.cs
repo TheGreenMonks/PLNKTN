71c80c7f2e2b0b248ae224bc94043a8d6a041197
Dexter Cunningham <dextercunningham@hotmail.co.uk>
2020-07-04 16:00:32 +0100

    Add Error Condition Unit Tests to UserServiceAddUserRewardToAllUsersShould

--- PLNKTNv2/PLNKTNv2.Tests/BusinessLogic/Services/UserServiceTests/UserServiceAddUserRewardToAllUsersShould.cs
+++ PLNKTNv2/PLNKTNv2.Tests/BusinessLogic/Services/UserServiceTests/UserServiceAddUserRewardToAllUsersShould.cs
@@ -4,6 +4,7 @@ using PLNKTNv2.BusinessLogic.Helpers.Implementation;
 using PLNKTNv2.BusinessLogic.Services;
 using PLNKTNv2.BusinessLogic.Services.Implementation;
 using PLNKTNv2.Models;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -42,6 +43,46 @@ namespace PLNKTNv2.Tests.BusinessLogic.Services.UserServiceTests
                 Assert.True(_user.UserRewards.Count() == 2);
                 Assert.True(_user.UserRewards[1].Id == "newRewardId");
             }
+        }
+
+        [Fact]
+        public void ErrorWithNullReward()
+        {
+            // Arrange
+            // Setup classes
+            IMessenger iMessengerMock = new Email();
+            IUserService sut = new UserService(iMessengerMock);
+
+            // Setup sut method parameters
+            ICollection<User> users = new List<User>();
+            Reward reward = null;
+            string jsonTemp = File.ReadAllText("./Json/Models/User.json");
+            User user = JsonConvert.DeserializeObject<User>(jsonTemp);
+            for (int i = 0; i < 3; i++)
+            {
+                user.Id = "string" + users.Count();
+                users.Add(user);
+            }
+
+            // Act Assert
+            Assert.ThrowsAny<NullReferenceException>(() => sut.AddUserRewardToAllUsers(reward, users));
+        }
+
+        [Fact]
+        public void ErrorWithNullUsers()
+        {
+            // Arrange
+            // Setup classes
+            IMessenger iMessengerMock = new Email();
+            IUserService sut = new UserService(iMessengerMock);
+
+            // Setup sut method parameters
+            ICollection<User> users = null;
+            string jsonTemp = File.ReadAllText("./Json/Models/Reward.json");
+            Reward reward = JsonConvert.DeserializeObject<Reward>(jsonTemp);
+
+            // Act Assert
+            Assert.ThrowsAny<NullReferenceException>(() => sut.AddUserRewardToAllUsers(reward, users));
         }
     }
 }
