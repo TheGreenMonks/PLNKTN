ad6207ae19c430762a2c84ed711bd526282e63cd
Dexter Cunningham <dextercunningham@hotmail.co.uk>
2020-07-08 10:22:53 +0100

    Bug Fix: Add Env Var to serverless.template for profile name to fix authentication exception. Add comment to explain local and live differentces in env var location.

--- PLNKTNv2/PLNKTNv2/BusinessLogic/Authentication/Account.cs
+++ PLNKTNv2/PLNKTNv2/BusinessLogic/Authentication/Account.cs
@@ -20,6 +20,11 @@ namespace PLNKTNv2.BusinessLogic.Authentication
         public bool TryGetLocalAwsCredentials(RegionEndpoint region, out AWSCredentials credentials)
         {
             var profileStore = new CredentialProfileStoreChain();
+            /* Info: Environment variables for the release/live version of this serverless app (i.e. when hosted on AWS) are defined in the serverless.template file
+             * under the lambda application.  For further details see https://docs.aws.amazon.com/serverless-application-model/latest/developerguide/sam-resource-function.html#sam-function-environment
+             * and https://docs.aws.amazon.com/AWSCloudFormation/latest/UserGuide/aws-properties-lambda-function-environment.html that describe how to add environment variables to the 
+             * live application. For local testing purposes env vars are still defined locally in the 'launchSettings.json'.
+             */
             if (profileStore.TryGetAWSCredentials(Environment.GetEnvironmentVariable("AWS_PROFILE_NAME"),
                 out AWSCredentials _credentials))
             {
