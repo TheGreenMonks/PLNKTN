3090731451a5cc6e0884fc607bcb9a9c29c825e1
Dexter Cunningham <dextercunningham@hotmail.co.uk>
2020-08-17 10:06:49 +0100

    Add Test Function to Test Adding Separate Lambda to ASP Project

---
+++ PLNKTNv2/PLNKTNv2/Function.cs
@@ -0,0 +1,18 @@
+using Amazon.Lambda.Core;
+
+namespace PLNKTNv2
+{
+    public class Function
+    {
+        /// <summary>
+        /// A simple function that takes a string and does a ToUpper
+        /// </summary>
+        /// <param name="input"></param>
+        /// <param name="context"></param>
+        /// <returns></returns>
+        public string FunctionHandler(string input, ILambdaContext context)
+        {
+            return input?.ToUpper();
+        }
+    }
+}
